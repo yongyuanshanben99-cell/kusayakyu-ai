c5c3747218c6e2de4ed176ac3dfc9944e6079177
yongyuanshanben99-cell <yongyuanshanben99@gmail.com>
2026-07-15 20:01:15 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,7 @@
-const CACHE="kusayakyu-ai-v41";
+const CACHE="kusayakyu-ai-v42";
 const ASSETS=["./","./index.html","./viewer.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
-self.addEventListener("install",event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
-});
-self.addEventListener("activate",event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
-});
+self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
+self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",event=>{
   if(event.request.mode==="navigate"){
     event.respondWith(fetch(event.request,{cache:"no-store"}).catch(()=>caches.match("./index.html")));
