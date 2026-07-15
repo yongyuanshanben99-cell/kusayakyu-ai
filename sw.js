9e73535f17aaae5ca11d7fb329e62acca1cd50e3
yongyuanshanben99-cell <yongyuanshanben99@gmail.com>
2026-07-15 20:14:54 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="kusayakyu-ai-v42";
+const CACHE="kusayakyu-ai-v43";
 const ASSETS=["./","./index.html","./viewer.html","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
